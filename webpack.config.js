5f5b4f9fd1b4ee5de8834119407cfa6752439405
rtukalenko <ruslan.tukalenko@gmail.com>
2018-08-01 10:47:21 +0300

    added static server

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+let path = require('path');
+let webpack = require('webpack');
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './src/main'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
