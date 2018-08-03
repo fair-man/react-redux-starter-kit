2c5fb79cde184dc9bdf862c9455aa16545fae0b4
rtukalenko <ruslan.tukalenko@gmail.com>
2018-08-03 13:31:09 +0300

    add constants

--- src/reducers/app.js
+++ src/reducers/app.js
@@ -1,3 +1,5 @@
+import {TYPED_NAME, CHANGE_NAME} from '../constants';
+
 const initialState = {
   user: 'unknown user',
   message: 'Welcome',
@@ -6,10 +8,10 @@ const initialState = {
 
 export default function app(state = initialState, action) {
   switch (action.type) {
-    case 'TYPED_NAME':
+    case TYPED_NAME:
       return {...state, newUserName: action.payload};
       break;
-    case 'CHANGE_NAME':
+    case CHANGE_NAME:
       return {...state, user: action.payload};
       break;
     default:
