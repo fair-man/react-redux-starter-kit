5814bf0b6da353bc1d5de8041e621d44648ad21d
rtukalenko <ruslan.tukalenko@gmail.com>
2018-08-03 12:44:39 +0300

    add flex-box-grid-css module

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -13,27 +13,25 @@ class App extends Component {
     this.props.appActions.changeName(this.props.app.newUserName);
   }
   render() {
-    return <div className="grid margin-16">
-        <div className="grid__box grid__box--size-12">
-          <div className="grid">
-            <div className="grid__box">{this.props.app.message} <b>{this.props.app.user}!</b> </div>
-          </div>
-        </div>
-        <div className="grid__box grid__box--size-12">
-          <div className="grid">
-            <div className="grid__box grid__box--align-self-flex-end">Введите новое имя:</div>
-            <div className="grid__box">
-              <input type="text"
-                     onChange={::this.typedName}
-                     defaultValue={this.props.app.newUserName}
-              />
-            </div>
-            <div className="grid__box">
-              <button type="button" onClick={::this.changeName}>Изменить</button>
-            </div>
-          </div>
-        </div>
+    return <div className="margin-16">
+      <div className="margin-8">
+        {this.props.app.message} <b>{this.props.app.user}</b>
       </div>
+      <div className="margin-8">
+        <span>
+          Введите новое имя:
+        </span>
+        <span>
+          <input type="text"
+                 onChange={::this.typedName}
+                 defaultValue={this.props.app.newUserName}
+          />
+        </span>
+        <span>
+          <button type="button" onClick={::this.changeName}>Изменить</button>
+        </span>
+      </div>
+    </div>
   }
 }
 
